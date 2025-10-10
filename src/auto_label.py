44aaed44d5360eb5ddb1c8376113bfc71915b242
Sarah Truc <sarahtruc@MacBook-Pro-cua-Sarah.local>
2025-10-10 12:06:28 +0700

    edit auto_label

--- src/auto_label.py
+++ src/auto_label.py
@@ -7,19 +7,18 @@ import shutil
 import tempfile
 
 def auto_label(image_dir, label_dir, model_path="yolov8n.pt", conf_thres=0.5, output_name="auto_label"):
-    project_dir = "runs/auto_label"
+    project_dir = "runs/"
     model = YOLO(model_path)
 
     print(f"Auto labeling images in {image_dir}...")
 
-    with tempfile.TemporaryDirectory() as temp_dir:
-        model.predict(
-            source = image_dir,
-            conf = conf_thres,
-            save_txt = True,
-            project = project_dir,
-            name = output_name
-        )
+    model.predict(
+        source = image_dir,
+        conf = conf_thres,
+        save_txt = True,
+        project = project_dir,
+        name = output_name
+    )
 
     gen_labels_dir = os.path.join(project_dir, output_name, "labels")
 
@@ -32,7 +31,18 @@ def auto_label(image_dir, label_dir, model_path="yolov8n.pt", conf_thres=0.5, ou
     for txt_file in os.listdir(gen_labels_dir):
         src_path = os.path.join(gen_labels_dir, txt_file)
         dst_path = os.path.join(label_dir, txt_file)
-        shutil.move(src_path, dst_path)
+        with open(src_path, "r") as f:
+            lines = f.readlines()
+
+        new_lines = []
+        for line in lines:
+            parts = line.strip().split()
+            if len(parts) >= 5:
+                parts[0] = "0"  # ép class thành 0 (face)
+                new_lines.append(" ".join(parts))
+
+        with open(dst_path, "w") as f:
+            f.write("\n".join(new_lines)) 
         print(f"[OK] {txt_file} -> {label_dir}")
 
     print(f"[DONE] Auto labeling for {image_dir} completed. Labels are saved in {label_dir}")
