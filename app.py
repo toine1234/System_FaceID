7a4296d08799efb536b2363b9af88411a349839b
Sarah Truc <sarahtruc@MacBook-Pro-cua-Sarah.local>
2025-10-13 10:12:20 +0700

    FaceDetect -> web

--- app.py
+++ app.py
@@ -1,30 +1,31 @@
 from flask import Flask, render_template, Response
 import cv2
+from src.face_detect import FaceDetector
 
 app = Flask(__name__)
+
+detector = FaceDetector("models/face_detector/yolov8n-face/train_results/weights/best.pt")
+
+def generate_frame():
+    cap = cv2.VideoCapture(0)
+    while True:
+        success, frame = cap.read()
+        if not success:
+            break
+
+        frame = detector.detect_faces(frame)
+        success, buffer = cv2.imencode('.jpg', frame)
+        frame = buffer.tobytes()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 @app.route('/')
 def index():
     return render_template('index.html')
-
-def gen():
-    cap = cv2.VideoCapture(0)
-
-    while True:
-        ret, frame = cap.read()
-        if not ret:
-            print("Error: failed to capture image")
-            break
-
-        ret, buffer = cv2.imencode('.jpg', frame)
-        frame = buffer.tobytes()
-
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-        
+      
 @app.route('/video_feed')
 def video_feed():
-    return Response(gen(),
+    return Response(generate_frame(),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
